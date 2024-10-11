7a91d343a96a0623d810f02669c17021e59bc5e5
Rajkumar. S <myidrajkumar@gmail.com>
2024-10-11 20:15:25 +0530

    Added AI with Dashboard

--- apis/dashboard_api.py
+++ apis/dashboard_api.py
@@ -1,6 +1,9 @@
 """Dashboard APIs"""
 
 from fastapi import APIRouter
+from langchain_core.output_parsers import PydanticOutputParser, StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel
 
 from db.connect import (
     get_candidate_experience_from_db,
@@ -14,9 +17,10 @@ from db.connect import (
     get_screening_interview_analytics_from_db,
     get_sourcing_analytics_from_db,
 )
-
+from llms.groq_llama_llm_versatile import load_llm
 
 router = APIRouter()
+llm = load_llm()
 
 
 @router.get("/analytics/kpis")
@@ -40,7 +44,19 @@ def get_sourcing_analytics():
     """Getting source analytics"""
 
     data = get_sourcing_analytics_from_db()
-    return {"message": "Success", "data": data}
+
+    message = """Sourcing data: {data}. This contains 'source_breakdown', 'geographical_sourcing'
+    and 'cost_per_source'. Please provide a brief summary of 'source_breakdown' and 'cost_per_source'.
+    DO NOT CONSIDER 'geographical_sourcing' data at all.
+    Please just provide summary in just 2 lines. Please provide in JSON format with the following fields
+
+    * source_breakdown_summary - 2 line summary
+    * cost_per_source_summary - 2 line summary
+    """
+
+    parser = PydanticOutputParser(pydantic_object=SourcingSummary)
+    summary = get_summary_from_llm(message, data, parser)
+    return {"message": "Success", "info": summary, "data": data}
 
 
 @router.get("/analytics/screeninginterview")
@@ -88,7 +104,15 @@ def get_compliance_metrics():
     """Get the compliance metrics"""
 
     data = get_compliance_metrics_from_db()
-    return {"message": "Success", "data": data}
+
+    message = """Compliance data: {data}. This contains 'gender_distribution', 'ethnicity_distribution'
+    and 'gdpr_compliance_rate'. Please provide insights and suggestions with 4 lines."""
+
+    parser = StrOutputParser()
+
+    summary = get_summary_from_llm(message, data, parser)
+
+    return {"message": "Success", "info": summary, "data": data}
 
 
 @router.get("/api/jobs")
@@ -97,3 +121,25 @@ def get_jobs():
 
     data = get_jobs_from_db()
     return {"message": "Success", "data": data}
+
+
+class SourcingSummary(BaseModel):
+    """Sourcing Summary Model"""
+
+    source_breakdown_summary: str
+    cost_per_source_summary: str
+
+
+def get_summary_from_llm(message, data, parser):
+    """Getting Summary from GenAI"""
+    prompt_template = ChatPromptTemplate(
+        [
+            (
+                "user",
+                message,
+            ),
+        ]
+    )
+
+    chain = prompt_template | llm | parser
+    return chain.invoke({"data": data})
---
+++ llms/groq_llama_llm_versatile.py
@@ -0,0 +1,17 @@
+"""Loading LLM"""
+
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+
+def load_llm():
+    """LLM Model to be used"""
+    llm = ChatGroq(
+        model="llama-3.1-70b-versatile",
+        api_key=os.getenv("GROQ_API_KEY"),
+        temperature=0.8,
+    )
+    return llm
